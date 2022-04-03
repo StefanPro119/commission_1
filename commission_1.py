a75a78fe204561b6f04dfce34ec8e6e27d855bfa
StefanPro119 <orlicstefan1990@gmail.com>
2022-04-03 12:35:28 +0200

    second commit

--- commission_1.py
+++ commission_1.py
@@ -1,5 +1,4 @@
-import datetime
-
+from datetime import datetime
 """
 High season: June - September => GuestReady commission is 15%
 Other months: commission is 10%
@@ -8,19 +7,20 @@ Write a function that takes a date string in the format 'YYYY-MM-DD' and returns
 get_commission(date_str) -> float
 """
 
-format = '%Y/%m/%d'
+date1 = '2021-06-01'
+date2 = '2021-09-01'
+june = datetime.strptime(date1, "%Y-%m-%d")
+september = datetime.strptime(date2, "%Y-%m-%d")
 
 def get_commission(data_str):
-    june = '2021/06/01'
-    september = '2021/09/01'
-    data_str = datetime.datetime.strptime(data_str, format)
-    june = datetime.datetime.strptime(june, format)
-    september = datetime.datetime.strptime(september, format)
-    if data_str >= june and data_str < september:
+    if data_str.month >= june.month and data_str.month < september.month:
         return 0.15
     return 0.1
 
-returned_commission = get_commission('2021/08/1')
+
+your_date = input('Insert the date in format (YYYY-MM-DD): ')
+converted_date = datetime.strptime(your_date, "%Y-%m-%d")
+returned_commission = get_commission(converted_date)
 
 if returned_commission < 0.15:
     print('Your given date is in low season and the commission is 10%')
